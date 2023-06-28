6fead7cf6891fa9305db2ea8100d2cdb951be9cf
Anthony Talarico <astalarico@gmail.com>
2023-06-28 13:31:13 -0400

    adding checks around the save trigger. setting the increment to 2 minutes

--- autosave.js
+++ autosave.js
@@ -1,6 +1,22 @@
 // set interval for every 5 minutes
 
-setInterval(function() {
-    document.getElementById('savebtn').click();
-}, 10000);
+window.onload = function () {
+    const clickEvent = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+    });
 
+    var saveBtn = document.getElementById("savebtn");
+        if (saveBtn.textContent == "Save") {
+            saveBtn.dispatchEvent(clickEvent);
+        }
+
+    setInterval(function () {
+
+        var saveBtn = document.getElementById("savebtn");
+        if (saveBtn.textContent == "Save") {
+            saveBtn.dispatchEvent(clickEvent);
+        }
+    }, 120000);
+};
